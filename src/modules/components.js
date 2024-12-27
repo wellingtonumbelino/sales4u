6fc574f76acd7b9e04f4def2f3cf791e7b7b6a9b
Wellington Umbelino <wellingtonsi@alu.ufc.br>
2024-12-26 23:20:33 -0300

    Create HeaderTitle component

---
+++ src/modules/components.js
@@ -0,0 +1,5 @@
+import HeaderTitle from "../components/templates/HeaderTitle.vue";
+
+export function setComponents(app) {
+  app.component("HeaderTitle", HeaderTitle);
+}
--- src/renderer.js
+++ src/renderer.js
@@ -7,6 +7,7 @@ import "./index.css";
 
 import router from "./router/router";
 
+import { setComponents } from "./modules/components";
 import { setPrimeVueComponents } from "./modules/primevue-components";
 
 const app = createApp(App);
@@ -18,6 +19,7 @@ app.use(PrimeVue, {
 });
 app.use(router);
 
+setComponents(app);
 setPrimeVueComponents(app);
 
 app.mount("#app");
