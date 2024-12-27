c035a8256b0920c88f50cbc3901ae92e7803491f
Wellington Umbelino <wellingtonsi@alu.ufc.br>
2024-12-26 23:20:43 -0300

    Create HeaderTitle component

---
+++ src/components/templates/HeaderTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2 class="header-title">{{ title }}</h2>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HeaderTitle",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
