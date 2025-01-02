9620589d4786bac8b05e8d898fa06053ffdd05ba
Wellington Umbelino <wellingtonsi@alu.ufc.br>
2025-01-02 07:44:33 -0300

    Install and use the Form from PrimeVue and install Zod to validate the form fields

--- src/components/views/product/ModalNewProduct.vue
+++ src/components/views/product/ModalNewProduct.vue
@@ -1,54 +1,121 @@
 <template>
-  <Dialog header="Novo Produto" modal v-model:visible="display">
-    <div class="form-new-product">
+  <Dialog
+    header="Novo Produto"
+    modal
+    v-model:visible="display"
+    :style="{ width: '20rem' }"
+  >
+    <Form
+      class="flex-column"
+      v-slot="$form"
+      :initialValue="initialValue"
+      :resolver="resolver"
+      @submit="onFormSubmit"
+    >
       <div class="p-input">
-        <label for="">Nome</label>
+        <label for="inputTextProductName">Nome</label>
         <InputText
+          id="inputTextProductName"
+          name="name"
           placeholder="Insira o nome do produto"
-          v-model="newProductModel.name"
         />
+        <Message
+          v-if="$form.name?.invalid"
+          severity="error"
+          size="small"
+          variant="simple"
+          >{{ $form.name.error?.message }}</Message
+        >
       </div>
       <div class="p-input">
-        <label for="">Preço</label>
+        <label for="inputNumberProductPrice">Preço</label>
         <InputNumber
           currency="BRL"
+          id="inputNumberProductPrice"
           locale="pt-BR"
           mode="currency"
-          v-model="newProductModel.price"
+          name="price"
           :min="0"
         />
+        <Message
+          v-if="$form.price?.invalid"
+          severity="error"
+          size="small"
+          variant="simple"
+          >{{ $form.price.error?.message }}</Message
+        >
       </div>
       <div class="p-input">
-        <label for="">Quantidade</label>
-        <InputNumber v-model="newProductModel.stock" :min="0" />
+        <label for="inputNumberProductStock">Quantidade</label>
+        <InputNumber id="inputNumberProductStock" name="quantity" :min="0" />
+        <Message
+          v-if="$form.quantity?.invalid"
+          severity="error"
+          size="small"
+          variant="simple"
+          >{{ $form.quantity.error?.message }}</Message
+        >
       </div>
-      <Button label="Cadastrar" />
-      <Button label="Fechar" />
-    </div>
+
+      <div class="flex-buttons">
+        <Button label="Cadastrar" type="submit" />
+        <Button class="p-button-outlined" label="Fechar" @click="hide" />
+      </div>
+    </Form>
   </Dialog>
 </template>
 
 <script>
+import { zodResolver } from "@primevue/forms/resolvers/zod";
 import { defineComponent, ref } from "vue";
+import { z } from "zod";
 
 export default defineComponent({
   name: "ModalNewProduct",
   setup() {
     let display = ref(false);
-    const newProductModel = ref({
+    const initialValue = ref({
       name: "",
       price: 0.0,
-      stock: 0,
+      quantity: 0,
     });
+    const resolver = ref(
+      zodResolver(
+        z.object({
+          name: z
+            .string()
+            .min(2, { message: "O nome do produto é obrigatório" }),
+          price: z
+            .number()
+            .min(0, { message: "O preço do produto é obrigatório" }),
+          quantity: z
+            .number()
+            .min(0, { message: "A quantidade do produto é obrigatória" }),
+        })
+      )
+    );
 
     const open = () => {
       display.value = true;
     };
 
+    const hide = () => {
+      display.value = false;
+    };
+
+    const onFormSubmit = ({ valid, values }) => {
+      if (!valid) return;
+
+      console.log(values);
+    };
+
     return {
       display,
-      newProductModel,
+      hide,
+      initialValue,
+      onFormSubmit,
       open,
+      resolver,
     };
   },
 });
--- src/modules/primevue-components.js
+++ src/modules/primevue-components.js
@@ -2,16 +2,20 @@ import Button from "primevue/button";
 import Column from "primevue/column";
 import DataTable from "primevue/datatable";
 import Dialog from "primevue/dialog";
+import { Form } from "@primevue/forms";
 import InputNumber from "primevue/inputnumber";
 import InputText from "primevue/inputtext";
 import Menu from "primevue/menu";
+import Message from "primevue/message";
 
 export function setPrimeVueComponents(app) {
   app.component("Button", Button);
   app.component("Column", Column);
   app.component("DataTable", DataTable);
   app.component("Dialog", Dialog);
+  app.component("Form", Form);
   app.component("InputNumber", InputNumber);
   app.component("InputText", InputText);
   app.component("Menu", Menu);
+  app.component("Message", Message);
 }
